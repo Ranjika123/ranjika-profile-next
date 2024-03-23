5027f4476d5173b81fcdaf74bd0a7a0c32c34b66
Ranjika Nethpriya <101783692+Ranjika123@users.noreply.github.com>
2024-03-23 21:30:40 +0530

    Update AboutSection.jsx

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -54,7 +54,7 @@ const AboutSection = () => {
   return (
     <section className="text-white flex: 1 1 auto" id="about">
       <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
-        <Image src="/images/About-Image.jpeg" width={600} height={600} />
+        <Image src="/images/About-Image.jpeg" width={600} height={600}  className="rounded-md shadow-md shadow-yellow-500/50 "/>
         <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
           <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
           <p className="text-base lg:text-lg ">
